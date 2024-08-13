f6cc1a0d1422a3fd96a3eb13beed468d4fea2bb5
Joel Wegman <joel.wegman@gmail.com>
2024-08-13 01:48:16 -0700

    implement most gameplay, clean up, better comments

--- src/jsMain/kotlin/reversi.kt
+++ src/jsMain/kotlin/reversi.kt
@@ -3,48 +3,122 @@ import org.w3c.dom.*
 import org.w3c.dom.Element
 import org.w3c.dom.events.*
 
-val boardState = mapOf(
-	"turn" to 1,
-	"score" to mapOf("1" to 2, "-1" to 2),
-	"grid" to listOf(
-		listOf( 0,  0,  0,  0,  0,  0,  0,  0),
-		listOf( 0,  0,  0,  0,  0,  0,  0,  0),
-		listOf( 0,  0,  0,  0,  0,  0,  0,  0),
-		listOf( 0,  0,  0,  1, -1,  0,  0,  0),
-		listOf( 0,  0,  0, -1,  1,  0,  0,  0),
-		listOf( 0,  0,  0,  0,  0,  0,  0,  0),
-		listOf( 0,  0,  0,  0,  0,  0,  0,  0),
-		listOf( 0,  0,  0,  0,  0,  0,  0,  0),
-	)
-)
+// The two players are represented internally as 1 and -1.
+// 0 represents an empty square.
+data class BoardState (var turn: Int, var grid: List<List<Int>>)
+var boardState = BoardState(1, listOf(
+	listOf( 0,  0,  0,  0,  0,  0,  0,  0),
+	listOf( 0,  0,  0,  0,  0,  0,  0,  0),
+	listOf( 0,  0,  0,  0,  0,  0,  0,  0),
+	listOf( 0,  0,  0,  1, -1,  0,  0,  0),
+	listOf( 0,  0,  0, -1,  1,  0,  0,  0),
+	listOf( 0,  0,  0,  0,  0,  0,  0,  0),
+	listOf( 0,  0,  0,  0,  0,  0,  0,  0),
+	listOf( 0,  0,  0,  0,  0,  0,  0,  0),
+))
 
-fun isValid(state: Map<String, Any>, x: Int, y: Int): Boolean {
-	return true
+// create a new state from and old one and the player's move coordinates
+fun newState(state: BoardState, x0: Int, y0: Int): BoardState {
+	data class Vec2 (var x: Int, var y: Int)
+
+	val directions = listOf(
+		Vec2(0, -1),  // ↑
+		Vec2(-1, -1), // ↖
+		Vec2(-1, 0),  // ←
+		Vec2(-1, 1),  // ↙
+		Vec2(0, 1),   // ↓
+		Vec2(1, 1),   // ↘
+		Vec2(1, 0),   // →
+		Vec2(1, -1))  // ↗
+
+	// helper function to get a list of coordinates starting at a given point
+	// and continuing along a given direction
+	fun getCoordsAlongRay(origin: Vec2, direction: Vec2): List<Vec2> {
+		var count = 0
+		var coords: MutableList<Vec2> = mutableListOf()
+		while (true) {
+			val x = origin.x + count * direction.x
+			val y = origin.y + count * direction.y
+			if (x >= 0 && x < 8 && y >= 0 && y < 8)
+				coords.add(Vec2(x, y))
+			else
+				break
+			count++
+		}
+		return coords
+	}
+
+	// get the list of all coords to change, starting with the player's move
+	val captures = directions.fold(setOf(Vec2(x0, y0))) { acc, dir ->
+		// then find the first coord with a piece belonging to the player
+		val origin = Vec2(x0 + dir.x, y0 + dir.y)
+		val allCoords = getCoordsAlongRay(origin, dir)
+		val playerIndex = allCoords.indexOfFirst( { state.grid[it.y][it.x] == state.turn } )
+
+		// if the move is invalid, don't capture anything, otherwise capture
+		// everything between the player's move and their opposing piece
+		val emptyIndex = allCoords.indexOfFirst( { state.grid[it.y][it.x] == 0 } )
+		val capturedCoords =
+			if (playerIndex < 0 || emptyIndex < playerIndex)
+				listOf()
+			else
+				allCoords.take((playerIndex))
+		acc union capturedCoords
+	}.distinct()
+
+	// return a new state with all captured pieces (including the player's move)
+	// toggled to the player, and the turn toggled to the other player
+	return BoardState(state.turn * -1, state.grid.mapIndexed { y, row ->
+		row.mapIndexed { x, it ->
+			if (captures.contains(Vec2(x, y))) state.turn else it
+		}
+	})
 }
 
+// check whether a move is valid within a given state
+fun isValid(state: BoardState, x0: Int, y0: Int): Boolean {
+	// coords already in use are invalid
+	if (state.grid[y0][x0] != 0) return false
+
+	// tentatively try the move and count how many pieces differ between it and
+	// the current state; the move is valid if and only if the count is greater
+	// than 1 (i.e. the move must capture some of the opponent's pieces)
+	val potentialState = newState(state, x0, y0)
+	var diffs = 0
+	for (y in 0..<8) {
+		for (x in 0..<8) {
+			if (state.grid[y][x] != potentialState.grid[y][x]) diffs++
+		}
+	}
+	return diffs > 1
+}
+
+// click event handler
 @JsExport
 fun moveTo(event: Event, element: HTMLElement): Unit {
 	event.preventDefault()
 	event.stopPropagation()
-	println("click")
+
+	// TODO: find a cleaner and/or more idiomatic way of doing this
+	val x = (element.attributes.getNamedItem("data-x")?.value?:"0").toIntOrNull()?:0
+	val y = (element.attributes.getNamedItem("data-y")?.value?:"0").toIntOrNull()?:0
+
+	boardState = newState(boardState, x, y)
+	render(boardState)
 }
 
-fun render(state: Map<String, Any>) {
+fun render(state: BoardState) {
+	// TODO: use a real templating engine instead of this mess
 	val rendered =
-		(state["grid"] as List<List<Int>>).mapIndexed { y, row ->
+		state.grid.mapIndexed { y, row ->
 			"""<div class="row">""" +
 			row.mapIndexed { x, item ->
-				val playerNumber = when (item) {
-					1 -> 0
-					-1 -> 1
-					else -> -1
-				}
 				val valid = isValid(state, x, y)
 				val link = """<a onclick="reversi.moveTo(event, this)" class="move" href="?x=$x&y=$y#board" data-x="$x" data-y="$y">"""
 				"""
 					${if (valid) link else ""}
-					<div class="square">
-						<div class="piece p${playerNumber} ${if (item != 0) "pip" else ""}"></div>
+					<div class="square ${if (valid) "valid" else ""}">
+						<div class="piece p${item} ${if (item != 0) "pip" else ""}"></div>
 					</div>
 					${if (valid) "</a>" else ""}
 				"""
